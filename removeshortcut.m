6d864cdd3a12724297a07a9289d5aa48fc38affc
Niru Maheswaranathan <nirum@stanford.edu>
2013-01-16 14:28:50 -0800

    bugfixes in removeshortcuts script

--- removeshortcut.m
+++ removeshortcut.m
@@ -3,7 +3,6 @@
 % Mon Sep 17 20:07:21 2012
 
 function removeshortcut(str)
-eval(['clearvars -except str; load shortcuts; clear ' str ';']);
 fprintf(['Removed ' str ' from shortcuts.\n']);
+eval(['clearvars -except ' str '; load shortcuts; clear ' str ';']);
 save('~/Documents/Code/functions/shortcuts.mat');
-clear; load shortcuts;
